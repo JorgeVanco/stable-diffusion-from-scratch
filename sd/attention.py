f7a994bbd647a78b2677f6c59fcfbbb172a68718
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-09 16:25:36 +0200

    feat: added cross-attention to attention.py

--- sd/attention.py
+++ sd/attention.py
@@ -78,3 +78,58 @@ class SelfAttention(nn.Module):
         output = self.out_proj(output)
 
         return output
+
+
+class CrossAttention(nn.Module):
+
+    def __init__(
+        self,
+        num_heads: int,
+        embed_dim: int,
+        cross_dim: int,
+        in_proj_bias: bool = True,
+        out_proj_bias: bool = True,
+    ) -> None:
+        super().__init__()
+        self.num_heads = num_heads
+        self.embed_dim = embed_dim
+        assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads"
+        self.head_dim = embed_dim // num_heads
+
+        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=in_proj_bias)
+        self.k_proj = nn.Linear(cross_dim, embed_dim, bias=in_proj_bias)
+        self.v_proj = nn.Linear(cross_dim, embed_dim, bias=in_proj_bias)
+        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=out_proj_bias)
+
+    def forward(self, x, y):
+        # x (latent): (batch_size, seq_len_Q, Dim_Q)
+        # y (context): (batch_size, cross_seq_len_KV, Dim_KV) = (batch_size, 77, 768)
+
+        input_shape = x.shape
+        batch_size, _, _ = input_shape
+
+        intermidiate_shape = (batch_size, -1, self.num_heads, self.head_dim)
+
+        # Multiply query by Wq
+        q = self.q_proj(x)
+        k = self.k_proj(y)
+        v = self.v_proj(y)
+        q = q.view(intermidiate_shape).transpose(
+            1, 2
+        )  # [batch_size, num_heads, seq_len_Q, head_dim]
+        k = k.view(intermidiate_shape).transpose(
+            1, 2
+        )  # [batch_size, num_heads, cross_seq_len_KV, head_dim]
+        v = v.view(intermidiate_shape).transpose(
+            1, 2
+        )  # [batch_size, num_heads, cross_seq_len_KV, head_dim]
+
+        weight = q @ k.transpose(
+            -2, -1
+        )  # [batch_size, num_heads, seq_len_Q, cross_seq_len_KV]
+        weight /= math.sqrt(self.head_dim)
+        weight = F.softmax(weight, dim=-1)
+        output = weight @ v
+        output = output.transpose(1, 2).contiguous().view(input_shape)
+        output = self.out_proj(output)
+        return output
