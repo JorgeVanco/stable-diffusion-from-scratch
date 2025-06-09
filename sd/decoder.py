803998333ea8e79002ec2311a5a828c90c4f30ec
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-09 13:19:32 +0200

    feat: VAE

--- sd/decoder.py
+++ sd/decoder.py
@@ -10,11 +10,31 @@ class VAE_AttentionBlock(nn.Module):
     def __init__(self, channels: int) -> None:
         super().__init__()
         self.group_norm = nn.GroupNorm(32, channels)
-        self.attention = SelfAttention(channels)
+        self.attention = SelfAttention(1, channels)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass through the attention block."""
-        return self.attention(x)
+        # x: (Batch_size, In_Channels, Height, Width)
+        residue = x
+        b, c, h, w = x.shape
+
+        # (Batch_size, In_Channels, Height, Width) -> (Batch_size, In_Channels, Height * Width)
+        x = x.view(b, c, h * w)
+
+        # (Batch_size, In_Channels, Height * Width) -> (Batch_size, Height * Width, In_Channels)
+        x = x.transpose(-1, -2)
+
+        # (Batch_size, Height * Width, In_Channels) -> (Batch_size, Height * Width, In_Channels)
+        x = self.attention(x)
+
+        # (Batch_size, Height * Width, In_Channels) -> (Batch_size, In_Channels, Height * Width)
+        x = x.transpose(-1, -2)
+
+        # (Batch_size, In_Channels, Height * Width) -> (Batch_size, In_Channels, Height, Width)
+        x = x.view(b, c, h, w)
+
+        x += residue
+        return x
 
 
 class VAE_ResidualBlock(nn.Module):
@@ -55,3 +75,51 @@ class VAE_ResidualBlock(nn.Module):
         x = self.conv_2(x)
 
         return x + self.residual_layer(residue)
+
+
+class VAE_Decoder(nn.Sequential):
+
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            # (Batch_size, 512, Height / 8, Width / 8) -> (Batch_size, 512, Height / 8,  Width / 8)
+            VAE_ResidualBlock(512, 512),
+            # (Batch_size, 512, Height / 8, Width / 8) -> (Batch_size, 512, Height / 4,  Width / 4)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            # (Batch_size, 512, Height / 4, Width / 4) -> (Batch_size, 512, Height / 2,  Width / 2)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+            # (Batch_size, 256, Height / 2, Width / 2) -> (Batch_size, 256, Height,  Width)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            nn.GroupNorm(32, 128),
+            nn.SiLU(),
+            # (Batch_size, 128, Height, Width) -> (Batch_size, 3, Height, Width)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through the decoder."""
+        # x: (Batch_size, In_Channels, Height / 8, Width / 8)
+
+        x /= 0.18215  # Normalize input
+
+        for module in self:
+            x = module(x)
+        return x
