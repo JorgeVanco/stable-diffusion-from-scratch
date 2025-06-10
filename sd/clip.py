fix: missing causal mask in attention

sd/clip.py
@@ -35,9 +35,9 @@ class CLIPLayer(nn.Module):
     def __init__(self, n_heads: int, embed_dim: int) -> None:
         super().__init__()
 
-        self.layer_norm_1 = nn.LayerNorm(embed_dim)
+        self.layernorm_1 = nn.LayerNorm(embed_dim)
         self.self_attention = SelfAttention(n_heads, embed_dim)
-        self.layer_norm_2 = nn.LayerNorm(embed_dim)
+        self.layernorm_2 = nn.LayerNorm(embed_dim)
         self.linear_1 = nn.Linear(embed_dim, embed_dim * 4)
         self.linear_2 = nn.Linear(embed_dim * 4, embed_dim)
 
@@ -52,12 +52,12 @@ class CLIPLayer(nn.Module):
         # (Batch_size, Sequence_length, Dim) -> (Batch_size, Sequence_length, Dim)
         residue = x
 
-        x = self.layer_norm_1(x)
-        x = self.self_attention(x)
+        x = self.layernorm_1(x)
+        x = self.self_attention(x, causal_mask=True)
         x += residue
 
         residue = x
-        x = self.layer_norm_2(x)
+        x = self.layernorm_2(x)
         x = self.linear_1(x)
 
         x = x * torch.sigmoid(1.702 * x)  # QuickGELU activation
@@ -76,13 +76,13 @@ class CLIP(nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
-        self.embedding = nn.Embedding(49408, 768, 77)
+        self.embedding = CLIPEmbedding(49408, 768, 77)
 
-        self.layers = nn.ModuleList([CLIPLayer(12, 768) for i in range(12)])
+        self.layers = nn.ModuleList([CLIPLayer(12, 768) for _ in range(12)])
 
-        self.layer_norm = nn.LayerNorm(768)
+        self.layernorm = nn.LayerNorm(768)
 
-    def forward(self, tokens: torch.LongTensor) -> torch.Tensor:
+    def forward(self, tokens: torch.LongTensor | torch.Tensor) -> torch.Tensor:
         """
         Forward pass through the CLIP model.
 
@@ -101,6 +101,6 @@ class CLIP(nn.Module):
             state = layer(state)
 
         # (Batch_size, Sequence_length, Dim)
-        state = self.layer_norm(state)
+        state = self.layernorm(state)
 
         return state